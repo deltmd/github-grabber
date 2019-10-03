2e08f719127f354d60329bb5e839c970932bf2ea
deltmd <remidy@gmail.com>
2019-10-03 13:03:19 -0600

    Updates

---
+++ precache-manifest.caee8cfdf898a1d61d2c143a1ac920ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b38295dd914f464de9da",
+    "url": "/github-grabber/static/js/runtime~main.b38295dd.js"
+  },
+  {
+    "revision": "36884fe81a5190e62ebd",
+    "url": "/github-grabber/static/js/main.36884fe8.chunk.js"
+  },
+  {
+    "revision": "98c8d80596ff13f90658",
+    "url": "/github-grabber/static/js/1.98c8d805.chunk.js"
+  },
+  {
+    "revision": "36884fe81a5190e62ebd",
+    "url": "/github-grabber/static/css/main.ea5d411b.chunk.css"
+  },
+  {
+    "revision": "618b27ce46375a35ba59426626555f27",
+    "url": "/github-grabber/index.html"
+  }
+];
